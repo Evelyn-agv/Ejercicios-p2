c1f2f9433a14b76d878fba711e8c153d28210cd3
Evelyn-agv <85769402+Evelyn-agv@users.noreply.github.com>
2021-06-14 19:52:57 -0500

    Add files via upload

---
+++ Clase 3/cicloFor.py
@@ -0,0 +1,41 @@
+class For:
+    def __init__(self):
+        pass
+
+    def usoFor(self):
+        datos=["Evelyn", 20, True]
+        numeros= (2,20,4,7,8,15.2)
+        docente= {"nombre": "Daniel", "Edad": 50, "FAC": "Faci"}
+        registroN= [(30,45), [15,71], (43,53)]
+        registroA= [{"nombre": "Maria", "Nota": 75}, {"nombre": "Carlos", "Nota": 86}, {"nombre": "Michael", "Nota": 96}]
+        # for i in range(5):  #rango(0,1,2,3,4)
+        #     print("i= {}" .format(i))
+        # for i in range(2,10):  
+        #     print("i= {}" .format(i))
+        # for i in range(4,10,2):  
+        #     print("i= {}" .format(i), end= " ")
+        # for i in range(12,2,-3):  
+        #     print("i= {}" .format(i), end= " ")
+    
+        contar= len(datos)
+        # j=0
+        # while j < contar:
+        #     print("while", datos[j])
+        #     j+=1
+        # for i in range(contar-1,-1,-1):
+        #     print("for", datos[i])
+        # for dato in datos:
+        #     print(dato)
+        # for dato in ['H','O','L', 'A', 'como','estas']:
+        #     print(dato)
+        # print("\nDiccionario de notas")
+        # for clave, valor in docente.items():
+        #     print(clave, ":", valor, end= " ")
+        
+        for alumnos in registroA:
+            for clave, valor in alumnos.items():
+                print(clave, ":", valor, end= " ")
+
+
+for1= For()
+for1.usoFor()
